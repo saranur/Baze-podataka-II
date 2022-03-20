88efc8024e00e5241c86b2cc867e993bf5458a55
Sara Nur <sara.nuredinovski@edu.fit.ba>
2022-03-20 13:00:58 +0100

    Upload Vježbe 2

---
+++ Vježbe/Vježba 2/Vjezba2.sql
@@ -0,0 +1,156 @@
+﻿--1. Kroz SQL kod kreirati bazu podataka Vjezba2
+CREATE DATABASE Vjezba2
+GO
+USE Vjezba2
+--2. U pomenutoj bazi kreirati šemu Prodaja
+GO
+CREATE SCHEMA Prodaja
+GO
+--3. U šemi prodaja kreirati tabele sa sljedećom strukturom:
+CREATE TABLE Prodaja.Autori
+(
+AutorID VARCHAR(11) CONSTRAINT PK_Autor PRIMARY KEY,
+Prezime VARCHAR(40) NOT NULL,
+Ime VARCHAR(20) NOT NULL,
+Telefon CHAR(12) DEFAULT 'nepoznato',
+Adresa VARCHAR(40),
+Drzava CHAR(2), --za polja fiksne duzine koristimo CHAR
+PostanskiBroj CHAR(5),
+Ugovor BIT NOT NULL
+)
+CREATE TABLE Prodaja.Knjige
+(
+KnjigaID VARCHAR(6) CONSTRAINT PK_Knjige PRIMARY KEY,
+Naziv VARCHAR(80) NOT NULL,
+Vrsta CHAR(12) NOT NULL,
+IzdavacID CHAR(4),
+Cijena MONEY,
+Biljeska VARCHAR(200),
+Datum DATETIME
+)
+--4. Upotrebom insert naredbe iz tabele Publishers baze Pubs izvršiti kreiranje
+--i insertovanje podataka u tabelu Izdavaci šeme Prodaja
+SELECT* --select* prikazuje sve zapise koje se nalaze u toj tabeli
+INTO Prodaja.Izdavaci --sve iz pubs.dbo.pub_info se kopiralo u Prodaja.Izdavaci
+FROM pubs.dbo.publishers
+--5. U kreiranoj tabeli Izdavaci provjeriti koje polje je primarni ključ
+ALTER TABLE Prodaja.Izdavaci
+ADD CONSTRAINT PK_Izdavac PRIMARY KEY (pub_id)
+--6. Povezati tabelu Izdavaci sa tabelom Knjige
+ALTER TABLE Prodaja.Knjige
+ADD CONSTRAINT FK_Knjiga_Izdavaci FOREIGN KEY(IzdavacID) REFERENCES Prodaja.Izdavaci(pub_id)
+--7. U šemu prodaja dodati tabelu sa sljedećom strukturom
+CREATE TABLE Prodaja.AutorNaslovi
+(
+AutorID VARCHAR(11) CONSTRAINT FK_AutorNaslov_Autori FOREIGN KEY REFERENCES Prodaja.Autori(AutorID),
+KnjigaID VARCHAR(6) CONSTRAINT FK_AutorNaslov_Knjige FOREIGN KEY REFERENCES Prodaja.Knjige(KnjigaID),
+AuOrd TINYINT,
+CONSTRAINT PK_AutorNaslov PRIMARY KEY(AutorID, KnjigaID) --u medjutabelama treba biti kompozitni kljuc, samo dodamo oba FK kljuca
+)
+--8. U kreirane tabele izvršiti insert podataka iz baze Pubs (Za polje biljeska tabele knjige na mjestima gdje je vrijednost NULL pohraniti „nepoznata vrijednost“)
+INSERT INTO Prodaja.Autori(AutorID,Prezime,Ime,Telefon,Adresa,Drzava,PostanskiBroj,Ugovor)
+SELECT A.au_id,A.au_fname, A.au_lname, A.phone, A.address, A.state, A.zip, A.contract --kada je plavo radi se o rezervisanoj rijeci, mozemo dodati [address] kako nam ne bi predstavljalo problem
+--Obzirom na to da smo u SELECT-u listali redom onako kako nam se nalazi u tabeli Autori nismo morali nista dodavati, u suprotnom bismo morali bismo navesti u zagradama kod INSERT INTO Prodaja.Autori
+FROM pubs.dbo.authors AS A --AS kad stavimo A. izlistace sva polja u tabeliINSERT INTO Prodaja.Knjige(KnjigaID,Naziv,Vrsta,IzdavacID,Cijena,Biljeska,Datum)
+
+INSERT INTO Prodaja.Knjige(KnjigaID,Naziv,Vrsta,IzdavacID,Cijena,Biljeska,Datum)
+SELECT T.title_id, T.title, T.type, T.pub_id, T.price, ISNULL(T.notes, 'nepoznata vrijednost'), T.pubdate
+FROM pubs.dbo.titles AS T
+
+INSERT INTO Prodaja.AutorNaslovi(KnjigaID,AutorID,AuOrd)
+SELECT TA.title_id, TA.au_id, TA.au_ord
+FROM pubs.dbo.titleauthor AS TA
+
+--DELETE 
+--FROM Prodaja.AutorNaslovi
+
+--9.	U tabeli autori nad kolonom Adresa promijeniti tip podatka na nvarchar (40)
+ALTER TABLE Prodaja.Autori
+ALTER COLUMN Adresa NVARCHAR(40) 
+
+--10.	Prikazati sve Autore čije ime počinje sa slovom A ili S
+SELECT*
+FROM Prodaja.Autori AS A	
+WHERE A.Ime LIKE 'A%' OR A.Ime LIKE 'S%'
+--11.	Prikazati knjige gdje cijena nije unesena
+
+SELECT* 
+FROM Prodaja.Knjige AS K
+WHERE K.Cijena IS NULL
+
+--12.	U bazi Vjezba2 kreirati šemu narudzbe
+GO 
+CREATE SCHEMA Narudzbe
+GO
+
+--13.	Upotrebom insert naredbe iz tabele Region baze Northwind izvršiti kreiranje i insertovanje podataka u tabelu Regije šeme narudžbe
+SELECT* 
+INTO Narudzbe.Regije
+FROM Northwind.dbo.Region
+--14.	Prikazati sve podatke koji se nalaze u tabeli Regije
+SELECT* 
+FROM Narudzbe.Regije
+--15.	U tabelu Regije insertovati zapis:
+--5    SE
+INSERT INTO Narudzbe.Regije
+VALUES (5,'SE')
+--16.	U tabelu Regije insertovati zapise:
+--6   NE
+--7   NW
+INSERT INTO Narudzbe.Regije
+VALUES (6, 'NE'),(7,'NW')
+--17.	Upotrebom insert naredbe iz tabele OrderDetails baze Northwind izvršiti kreiranje i insertovanje podataka u tabelu StavkeNarudzbe šeme Narudzbe
+SELECT*
+INTO Narudzbe.StavkeNarudzbe
+FROM Northwind.dbo.[Order Details]
+--18.	U tabeli StavkeNarudzbe dodati standardnu kolonu ukupno tipa decimalni broj (8,2).
+ALTER TABLE Narudzbe.StavkeNarudzbe
+ADD Ukupno DECIMAL (8,2) 
+--19.	Izvršiti update kreirane kolone kao umnožak kolona Quantity i UnitPrice.
+UPDATE Narudzbe.StavkeNarudzbe
+SET Ukupno=Quantity*UnitPrice
+
+SELECT* 
+FROM Narudzbe.StavkeNarudzbe
+--20.	U tabeli StavkeNarduzbe dodati izračunatu (stalno pohranjenu) kolonu CijeliDio u kojoj će biti cijeli dio iz kolone UnitPrice
+ALTER TABLE Narudzbe.StavkeNarudzbe
+ADD CijeliDio AS FLOOR(UnitPrice)
+--21.	U tabeli StavkeNarduzbe kreirati ograničenje na koloni Discount kojim će se onemogućiti unos vrijednosti manjih od 0.
+ALTER TABLE Narudzbe.StavkeNarudzbe
+ADD CONSTRAINT CK_Discount CHECK(Discount>=0)
+--22.	U tabelu StavkeNarudzbe insertovati novi zapis (potrebno je )
+INSERT INTO Narudzbe.StavkeNarudzbe(OrderID, ProductID, UnitPrice,Quantity,Discount)
+VALUES(11,222,22,66,0)
+
+SELECT*
+FROM Narudzbe.StavkeNarudzbe AS SN
+WHERE SN.OrderID=11
+--23.	U šemu narudzbe dodati tabelu sa sljedećom strukturom:
+--Kategorije
+--•	KategorijaID ,  cjelobrojna vrijednost, primarni ključ i autoinkrement
+--•	ImeKategorije, 15 UNICODE znakova (obavezan unos)
+--•	Opis, tekstualan UNICODE tip podatka 
+CREATE TABLE Narudzbe.Kategorije
+(
+KategorijaID INT CONSTRAINT PK_Narudzba PRIMARY KEY IDENTITY(1,1),
+ImeKategorije NVARCHAR(15) NOT NULL,
+Opis NTEXT 
+)
+--24.	U kreiranu tabelu izvršiti insertovanje podataka iz tabele Categories baze Northwind
+SET IDENTITY_INSERT Narudzbe.Kategorije ON --Iskljuci nas Identity dok mi ne insertujemo podatke 
+INSERT INTO Narudzbe.Kategorije(KategorijaID,ImeKategorije,Opis)
+SELECT C.CategoryID, C.CategoryName, C.Description
+FROM Northwind.dbo.Categories AS C
+SET IDENTITY_INSERT Narudzbe.Kategorije OFF -- ovjde ga opet aktiviramo i on ce nastaviti od zadnjeg unijetog podatka 
+--25.	U tabelu Kategorije insertovati novu kategoriju pod nazivom „Ncategory“
+INSERT INTO Narudzbe.Kategorije(ImeKategorije)
+VALUES('Ncategory')
+--26.	Kreirati upit kojim će se prikazati sve kategorije
+SELECT*
+FROM Narudzbe.Kategorije
+--27.	Izvršiti update zapisa u tabeli Kategorija na mjestima gdje Opis kategorije nije dodan pohraniti vrijednost „bez opisa“
+UPDATE Narudzbe.Kategorije 
+SET Opis='bez opisa'
+WHERE Opis IS NULL
+--28.	Izvršiti brisanje svih kategorija
+DELETE FROM Narudzbe.Kategorije
---
+++ Vježbe/Vježba 2/ZadaciZaVjezbu2.sql
@@ -0,0 +1,159 @@
+﻿--1. Kroz SQL kod kreirati bazu podataka ZadaciZaVjezbu2
+CREATE DATABASE ZadaciZaVjezbu2
+GO 
+USE ZadaciZaVjezbu2
+--2. U pomenutoj bazi kreirati šemu Prodaja
+GO 
+CREATE SCHEMA Prodaja
+GO
+--3. U kreiranoj bazi podataka kreirati tabele sa sljedećom strukturom:
+--a) Proizvodi
+-- ProizvodID, cjelobrojna vrijednost, autoinkrement i primarni ključ
+-- Naziv, 40 UNICODE karaktera (obavezan unos)
+-- Cijena, novčani tip
+-- KolicinaNaSkladistu, skraćeni cjelobrojni tip
+-- Raspolozivost, bit polje (obavezan unos)
+CREATE TABLE Prodaja.Proizvodi
+(
+ProizvodID INT CONSTRAINT PK_Proizvod PRIMARY KEY IDENTITY(1,1),
+Naziv NVARCHAR(40) NOT NULL,
+Cijena MONEY,
+KolicinaNaSkladistu SMALLINT, 
+Raspolozivost BIT NOT NULL
+)
+--b) Kupci
+-- KupciID, 5 UNICODE fiksna karaktera i primarni ključ
+-- NazivKompanije, 40 UNICODE karaktera (obavezan unos)
+-- Ime, 30 UNICODE karaktera
+-- Telefon, 24 UNICODE karaktera
+-- Faks, 24 UNICODE karaktera
+CREATE TABLE Prodaja.Kupci
+(
+KupciID NCHAR(5) CONSTRAINT PK_Kupci PRIMARY KEY, 
+NazivKompanije NVARCHAR(40) NOT NULL, 
+Ime NVARCHAR(30),
+Telefon NVARCHAR(24),
+Faks NVARCHAR(24)
+)
+--c) Narudzbe
+-- NarudzbaID, cjelobrojna vrijednost, autoinkrement i primarni ključ,
+-- DatumNarudzbe, polje za unos datuma
+-- DatumPrijema, polje za unos datuma
+-- DatumIsporuke, polje za unos datuma
+-- Drzava, 15 UNICODE karaktera
+-- Regija, 15 UNICODE karaktera
+-- Grad, 15 UNICODE karaktera
+-- Adresa, 60 UNICODE karaktera
+CREATE TABLE Prodaja.Narudzbe
+(
+NarudzbaID INT CONSTRAINT PK_Narudzbe PRIMARY KEY IDENTITY(1,1),
+DatumNarudzbe DATE, 
+DatumPrijema DATE, 
+DatumIsporuke DATE, 
+Drzava NVARCHAR(15), 
+Regija NVARCHAR(15),
+Grad NVARCHAR(15), 
+Adresa NVARCHAR(60)
+)
+--d) StavkeNarudzbe
+-- NarudzbaID, cjelobrojna vrijednost, strani ključ
+-- ProizvodID, cjelobrojna vrijednost, strani ključ
+-- Cijena, novčani tip (obavezan unos),
+-- Kolicina, skraćeni cjelobrojni tip (obavezan unos) i defaultna vrijednost (1),
+-- Popust, realna vrijednost (obavezan unos)
+-- VrijednostStavki narudžbe (uzeti u obzir i popust)- calculated polje
+--**Definisati primarni ključ tabele
+CREATE TABLE Prodaja.StavkeNarudzbe
+(
+NarudzbaID INT CONSTRAINT FK_StavkeNarudzbe_Narudzba FOREIGN KEY REFERENCES Prodaja.Narudzbe(NarudzbaID),
+ProizvodID INT CONSTRAINT FK_StavkeNarudzbe_Proizvod FOREIGN KEY REFERENCES Prodaja.Proizvodi(ProizvodID),
+Cijena MONEY NOT NULL, 
+Kolicina SMALLINT NOT NULL DEFAULT(1),
+Popust REAL NOT NULL, 
+VrijednostStavki AS (Cijena*Kolicina*(1-Popust)),
+CONSTRAINT PK_StavkeNarudzbe PRIMARY KEY (NarudzbaID, ProizvodID)
+)
+
+--4. Iz baze podataka Northwind u svoju bazu podataka prebaciti sljedeće podatke:
+--a) U tabelu Proizvodi dodati sve proizvode 
+-- ProductID -> ProizvodID
+-- ProductName -> Naziv 
+-- UnitPrice -> Cijena 
+-- UnitsInStock -> KolicinaNaSkladistu
+-- Discontinued -> Raspolozivost 
+
+SET IDENTITY_INSERT Prodaja.Proizvodi ON 
+INSERT INTO Prodaja.Proizvodi(ProizvodID,Naziv,Cijena,KolicinaNaSkladistu,Raspolozivost)
+SELECT P.ProductID, P.ProductName, P.UnitPrice, P.UnitsInStock, P.Discontinued
+FROM Northwind.dbo.Products AS P
+SET IDENTITY_INSERT Prodaja.Proizvodi OFF
+--b) U tabelu Kupci dodati sve kupce
+-- CustomerID -> KupciID
+-- CompanyName -> NazivKompanije
+-- ContactName -> Ime
+-- Phone -> Telefon
+-- Fax -> Faks
+INSERT INTO Prodaja.Kupci(KupciID, NazivKompanije, Ime, Telefon,Faks)
+SELECT C.CustomerID, C.CompanyName, C.ContactName, C.Phone, C.Fax
+FROM Northwind.dbo.Customers AS C
+
+--c) U tabelu Narudzbe dodati sve narudžbe, na mjestima gdje nema pohranjenih podataka o regiji 
+--zamijeniti vrijednost sa nije naznaceno
+-- OrderID -> NarudzbaID
+-- OrderDate -> DatumNarudzbe
+-- RequiredDate -> DatumPrijema
+-- ShippedDate -> DatumIsporuke
+-- ShipCountry -> Drzava
+-- ShipRegion -> Regija
+-- ShipCity -> Grad
+-- ShipAddress -> Adresa
+SET IDENTITY_INSERT Prodaja.Narudzbe ON
+INSERT INTO Prodaja.Narudzbe(NarudzbaID,DatumNarudzbe,DatumPrijema,DatumIsporuke,Drzava,Regija,Grad,Adresa)
+SELECT O.OrderID, O.OrderDate, O.RequiredDate, O.ShippedDate, O.ShipCountry, O.ShipRegion, O.ShipCity, O.ShipAddress
+FROM Northwind.dbo.Orders AS O
+SET IDENTITY_INSERT Prodaja.Narudzbe OFF
+
+--d) U tabelu StavkeNarudzbe dodati sve stavke narudžbe gdje je količina veća od 4
+-- OrderID -> NarudzbaID
+-- ProductID -> ProizvodID
+-- UnitPrice -> Cijena
+-- Quantity -> Kolicina
+-- Discount -> Popust
+INSERT INTO Prodaja.StavkeNarudzbe(NarudzbaID,ProizvodID,Cijena,Kolicina,Popust)
+SELECT OD.OrderID, OD.ProductID, OD.UnitPrice, OD.Quantity, OD.Discount
+FROM Northwind.dbo.[Order Details] AS OD
+WHERE OD.Quantity>4
+
+SELECT* 
+FROM Prodaja.StavkeNarudzbe
+
+--5. Kreirati upit kojim će se prikazati svi proizvodi čija je cijena veća od 100
+SELECT* 
+FROM Prodaja.Proizvodi AS P
+WHERE P.Cijena>100
+--6. Insert komandom dodati novi proizvod 
+INSERT INTO Prodaja.Proizvodi(Naziv, Raspolozivost)
+VALUES('novi proizvod', 1)
+SELECT*
+FROM Prodaja.Proizvodi AS PP
+WHERE PP.Naziv='novi proizvod'
+--7. Dodati novu stavku narudzbe
+INSERT INTO Prodaja.StavkeNarudzbe(NarudzbaID, ProizvodID, Cijena,Popust)
+VALUES(10248,78,4,0)
+
+SELECT*
+FROM Prodaja.StavkeNarudzbe
+--8. Izbrisati sve stavke narudzbe gdje je narudzbaID 10248
+DELETE 
+FROM Prodaja.StavkeNarudzbe
+WHERE NarudzbaID=10248
+--9. U tabeli Proizvodi kreirati ograničenje na koloni Cijena kojim će se onemogućiti unos vrijednosti manjih od 0,1
+ALTER TABLE Prodaja.Proizvodi 
+ADD CONSTRAINT CK_Cijena CHECK(Cijena>=0.1)
+--10. U tabeli proizvodi dodati izračunatu kolonu pod nazivom potrebnoNaruciti za količinu 
+--proizvoda na skladištu ispod 10 potrebno je pohraniti vrijednost „DA“ a u suptornom „NE“
+ALTER TABLE Prodaja.Proizvodi 
+ADD potrebnoNaruciti AS CASE WHEN(KolicinaNaSkladistu<10) THEN 'DA' ELSE 'NE' END
+
+SELECT*
+FROM Prodaja.Proizvodi
